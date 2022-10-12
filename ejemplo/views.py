bde165efb69333a716b255eddcaf20b819afaccb
Youkaidan <ekop92@outlook.com>
2022-10-11 23:26:50 -0300

    agrego ejercicio imc

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Erwin"})
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    {
+        "notas":[1,2,3,4,5,6,7,8]
+    })
+
+def imc(request, peso, altura):
+    imc = int(peso)*(int(altura)*int(altura)) #calcular el imc
+    return render(request, "ejemplo/imc.html", {"imc":imc})
