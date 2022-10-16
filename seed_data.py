bda4118b7e02d3fe904ab9f11eeda0f1d92edd94
Youkaidan <ekop92@outlook.com>
2022-10-16 11:25:50 -0300

    MTV 2

--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,10 @@
 from ejemplo.models import Familiar
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+#Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
+#Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
+#Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
+#Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+
+Familiar(nombre="Erwin", direccion="Calle uno 222", numero_pasaporte=654321).save()
 
 print("Se cargo con éxito los usuarios de pruebas")
