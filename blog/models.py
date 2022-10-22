e7a1475ad7abfc963f578605d0769713109d4c1e
Youkaidan <ekop92@outlook.com>
2022-10-22 16:13:06 -0300

    seed

--- blog/models.py
+++ blog/models.py
@@ -2,24 +2,25 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Configuracion(models.Model):
-    nombre_blog = models.CharField(max_length=14)
-    construido_por = models.CharField(max_length=30)
+    nombre_blog = models.CharField(max_length=14, default='')
+    construido_por = models.CharField(max_length=30, default='')
     titulo_principal = models.CharField(max_length=30, default='')
     subtitulo_principal = models.CharField(max_length=60, default='')
 
 class Juego(models.Model):
-    nombre_juego = models.CharField(max_length=30)
-    desarrollador = models.CharField(max_length=30)
+    nombre_juego = models.CharField(max_length=30, default='')
+    desarrollador = models.CharField(max_length=30, default='')
     genero = models.CharField(max_length=30, default='')
     cantidad_jugadores = models.CharField(max_length=30, default='')
-    reseña = models.CharField(max_length=100, default='')
+    reseña = models.CharField(max_length=200, default='')
 
 class Desarrollador(models.Model):
-    nombre_desarrollador = models.CharField(max_length=30)
-    pais = models.CharField(max_length=30)
+    nombre_desarrollador = models.CharField(max_length=30, default='')
+    pais = models.CharField(max_length=30, default='')
     anno_fundacion = models.CharField(max_length=30, default='')
 
 class Noticias(models.Model):
-    titulo = models.CharField(max_length=60)
-    sub_titulo = models.CharField(max_length=60)
-    contenido = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100, default='')
+    sub_titulo = models.CharField(max_length=80, default='')
+    contenido = models.CharField(max_length=300, default='')
+    fecha = models.CharField(max_length=30, default='')
--- seed_data.py
+++ seed_data.py
@@ -9,4 +9,20 @@ from blog.models import Configuracion, Juego, Desarrollador, Noticias
 
 Configuracion(nombre_blog="Blog Gamer", construido_por="Erwin Orellana", titulo_principal="Reseñas de Videojuegos", subtitulo_principal="En éste blog encontraras reseñas y noticias de Videojuegos.").save()
 
+Juego(nombre_juego="Cyberpunk 2077", 
+desarrollador="CD Projekt Red", 
+genero="Disparos/Rol", 
+cantidad_jugadores="Un jugador", 
+reseña="La historia sigue la lucha de V mientras lidia con un misterioso implante cibernético que amenaza con sobrescribir su cuerpo con la personalidad y los recuerdos de una celebridad fallecida que solo V percibe; los dos deben trabajar juntos si hay alguna esperanza de separarlos y salvar la vida de V.").save()
+
+Desarrollador(nombre_desarrollador="CD Projekt Red", 
+pais="Polonia", 
+anno_fundacion="1995").save()
+
+Noticias(titulo="CD Projekt Red festeja", 
+sub_titulo="Cyberpunk 2077 llegó al millón de jugadores diarios en el último mes", 
+contenido="El estudio polaco celebró el hito en redes sociales y le agradeció a todos los jugadores que le han brindado su apoyo.",
+fecha="21/10/2022").save()
+
+
 print("Se cargo con éxito la data base")
